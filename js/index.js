3ec8d4072e6800c8f068156078b1d1452a9c58bb
BreElle Wells <breellejordyn@gmail.com>
2023-07-23 16:58:32 -0600

    complete deliverable 3

--- js/index.js
+++ js/index.js
@@ -4,14 +4,14 @@ let inputValue;
 const inputForm = document.getElementById('github-form')
 inputForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    let inputValue = document.getElementById('search').value
+    inputValue = document.getElementById('search').value
     searchUsers(inputValue)
 })
 
 const incompleteUrl = 'https://api.github.com/search/users?q='
 
 function searchUsers(inputValue) {
-    fetch(incompleteUrl + `${inputValue}`)
+    fetch(incompleteUrl + inputValue)
     .then(r => r.json())
     .then(users => renderData(users.items))
 }
@@ -21,15 +21,27 @@ const userList = document.getElementById('user-list')
 // deliverable 2: display user information
 function renderData(users) {
     users.forEach(userObject => {
-        
+        console.log(userObject)
         const userAvatar = document.createElement('img')
         userAvatar.src = userObject.avatar_url
         
         const userContainer = document.createElement('li')
         userContainer.innerText = userObject.login
+        userContainer.addEventListener('click', () => fetchRepoData(userObject.login))
         
         userContainer.appendChild(userAvatar)
 
         userList.appendChild(userContainer)
     })
 }
+
+const incompleteRepoUrl = 'https://api.github.com/users/'
+
+// deliverable 3: click on user to return repository data
+function fetchRepoData(userLogin) {
+    fetch(incompleteRepoUrl + userLogin + '/repos')
+    .then(r => r.json())
+    .then(repos => renderRepos(repos))
+}
+
+// deliverable 4: display all repos for user
